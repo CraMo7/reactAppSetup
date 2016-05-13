aad207cde82ed169e9d6fed58faac3ce66c8bf0e
Craig Morton <CraigMorton92@gmail.com>
2016-05-13 01:46:04 +0100

    Basic React app setup

---
+++ client/src/components/Main.jsx
@@ -0,0 +1,19 @@
+var React = require("react");
+
+var Main = React.createClass({
+  render: function(){
+    return (
+      <div>
+
+      </div>
+      );
+  },
+  getInitialState: function(){
+    return {};
+  },
+  getDefaultProps: function(){
+    return {};
+  }
+});
+
+module.exports = Main;
---
+++ client/src/index.js
@@ -0,0 +1,9 @@
+var ReactDOM = require("react-dom");
+var Main = require("../components/Main.jsx")
+
+window.onload = function(){
+  ReactDOM.render(
+    <Main/>
+    document.getElementById("main")
+    );
+};
